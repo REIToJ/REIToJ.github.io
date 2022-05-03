acb77005a2421b5d371b0b05dbe58abe000eb5f5
REIToJ <vladrest60@gmail.com>
2022-05-03 17:33:05 +0300

    Commit

--- todo-list-dstu/src/main.js
+++ todo-list-dstu/src/main.js
@@ -1,10 +1,5 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
 
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
-
-createApp(App).use(store).use(router).mount("#app");
-router.push("/sign-in");
+createApp(App).use(router).mount('#app')
--- todo-list-dstu/src/router/index.js
+++ todo-list-dstu/src/router/index.js
@@ -1,41 +1,18 @@
-import { createRouter, createWebHistory } from "vue-router";
-import SignInView from "@/views/SignInView";
-import store from "@/store";
+import { createRouter, createWebHashHistory } from 'vue-router'
+import Home from '../views/Home.vue'
 
 const routes = [
   {
-    path: "/sign-in",
-    name: "sign-in",
-    component: SignInView,
+    path: '/',
+    name: 'home',
+    component: Home
   },
-  {
-    path: "/sign-up",
-    name: "sign-up",
-    component: () => import("@/views/SignUpView"),
-  },
-  {
-    path: "/home",
-    name: "home",
-    component: () => import("@/views/HomeView"),
-    meta: { requiresAuth: true },
-  },
-];
+  
+]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes,
-});
+  history: createWebHashHistory(),
+  routes
+})
 
-router.beforeEach(async (to, from, next) => {
-  if (to.matched.some((record) => record.meta.requiresAuth)) {
-    if (store.getters.isAuthenticated) {
-      next();
-      return;
-    }
-    next("/sign-in");
-  } else {
-    next();
-  }
-});
-
-export default router;
+export default router
